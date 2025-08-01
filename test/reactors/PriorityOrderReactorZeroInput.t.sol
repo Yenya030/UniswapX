32f53f944e178d10bc85585e3ce4092ce74ce27d
Yenya030 <yenya030@gmail.com>
2025-08-01 12:24:32 -0500

    test: check priority order zero input

---
+++ test/reactors/PriorityOrderReactorZeroInput.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {PriorityOrderReactorTest} from "./PriorityOrderReactor.t.sol";
+import {PriorityOrderLib, PriorityOrder, PriorityInput, PriorityOutput, PriorityCosignerData} from "../../src/lib/PriorityOrderLib.sol";
+import {SignedOrder, OrderInfo} from "../../src/base/ReactorStructs.sol";
+import {OutputsBuilder} from "../util/OutputsBuilder.sol";
+import {OrderInfoBuilder} from "../util/OrderInfoBuilder.sol";
+import {NATIVE} from "../../src/lib/CurrencyLibrary.sol";
+import {ERC20} from "solmate/src/tokens/ERC20.sol";
+
+contract PriorityOrderReactorZeroInputTest is PriorityOrderReactorTest {
+    using OrderInfoBuilder for OrderInfo;
+    using PriorityOrderLib for PriorityOrder;
+
+    function testExecuteZeroInput() public {
+        PriorityCosignerData memory cosignerData = PriorityCosignerData({auctionTargetBlock: block.number});
+        PriorityOrder memory order = PriorityOrder({
+            info: OrderInfoBuilder.init(address(reactor)).withSwapper(swapper),
+            cosigner: vm.addr(cosignerPrivateKey),
+            auctionStartBlock: block.number,
+            baselinePriorityFeeWei: 0,
+            input: PriorityInput({token: ERC20(address(NATIVE)), amount: 0, mpsPerPriorityFeeWei: 0}),
+            outputs: OutputsBuilder.singlePriority(address(tokenOut), ONE, 0, swapper),
+            cosignerData: cosignerData,
+            cosignature: bytes("")
+        });
+        order.cosignature = _cosign(order.hash(), cosignerData);
+        bytes memory sig = signOrder(swapperPrivateKey, address(permit2), order);
+        fillContract.execute(SignedOrder(abi.encode(order), sig));
+        assertEq(tokenOut.balanceOf(address(fillContract)), 0);
+        assertEq(tokenOut.balanceOf(address(swapper)), ONE);
+    }
+
+    function _cosign(bytes32 orderHash, PriorityCosignerData memory cosignerData) internal view returns (bytes memory sig) {
+        bytes32 msgHash = keccak256(abi.encodePacked(orderHash, block.chainid, abi.encode(cosignerData)));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(cosignerPrivateKey, msgHash);
+        sig = bytes.concat(r, s, bytes1(v));
+    }
+}
